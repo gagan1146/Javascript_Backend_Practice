09a90559df5952cb26ef6b2ad0ab9b156a0a9480
gagan1146 <gagansharma99690@gmail.com>
2025-10-09 20:44:48 +0530

    Syncronous File Handling

---
+++ File Handling Javascript/sync-file-handling.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+fs.writeFileSync("example.txt", "Hello, this is my first file!");
+const data = fs.readFileSync("example.txt", "utf-8");
+console.log("File content:", data);
+fs.appendFileSync("example.txt", "\nAdding more content here...");
+console.log("New content added!");
+const newData = fs.readFileSync("example.txt", "utf-8");
+console.log("Updated content:\n", newData);
+fs.unlinkSync("example.txt");
+console.log("File deleted successfully!");
